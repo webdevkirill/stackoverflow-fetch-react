4ac104728ba7f90dea53947a5bc1d3f1f36faa16
webdevkirill <kirill-fjodorov000@yandex.ru>
2021-03-30 22:22:30 +0300

    Добавляет компонент отдельного поста

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import { PostStyled } from './PostStyled';
 
-export default function Post() {
-	return <div>Post</div>;
+export default function Post({ post }) {
+	const {
+		link,
+		owner: { profile_image = '' },
+		title,
+	} = post;
+	return <PostStyled href={link}>{title}</PostStyled>;
 }
---
+++ src/components/Post/PostStyled.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const PostStyled = styled.a`
+	display: flex;
+	align-items: center;
+	cursor: pointer;
+	min-height: 40px;
+	padding: 8px;
+	width: 100%;
+	max-width: 500px;
+	font-size: 16px;
+	text-decoration: none;
+	color: #000;
+	margin-bottom: 32px;
+	border: 1px solid #000;
+`;
