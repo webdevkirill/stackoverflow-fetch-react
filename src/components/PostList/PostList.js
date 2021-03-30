71b90119886e5d655923dd9065f315dd11cdbe72
webdevkirill <kirill-fjodorov000@yandex.ru>
2021-03-30 22:05:56 +0300

    Добавляет фильтрацию списка постов

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import filter from 'lodash/filter';
 
 export default function PostList({ posts }) {
+	const filteredPosts = filter(posts, (post) => {
+		return post.is_answered && post.owner.reputation >= 50;
+	});
+	console.log(filteredPosts);
+
 	return <div>PostList</div>;
 }
